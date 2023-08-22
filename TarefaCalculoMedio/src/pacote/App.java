8e213649e2bdf0a5dd40ea1df9f49b5a65d9d974
Pedro Camargo <081camargopedro@gmail.com>
2023-08-21 21:14:00 -0300

    adicionando exercicio modulo 8

---
+++ TarefaCalculoMedio/src/pacote/App.java
@@ -0,0 +1,17 @@
+package pacote;
+
+public class App {
+
+    public static void main(String args[]){
+
+        int nota1 = 8;
+        int nota2 = 7;
+        int nota3 = 7;
+        int nota4 = 10;
+
+        int mediaNotas = (nota1+nota2+nota3+nota4)/4;
+        System.out.println(mediaNotas);
+
+    }
+
+}
