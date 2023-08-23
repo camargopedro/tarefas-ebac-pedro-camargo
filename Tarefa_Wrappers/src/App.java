43600dde02fc43359fd9f6445e0edaef12de438c
Pedro Camargo <081camargopedro@gmail.com>
2023-08-23 17:09:17 -0300

    adicionando Tarefa_Wrapper

---
+++ Tarefa_Wrappers/src/App.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String args[]){
+
+        Scanner s = new Scanner(System.in);
+        System.out.println("Digite um numero de 1 a 100: ");
+        int numPrimitivo = s.nextInt();
+        System.out.println("numero digitado: " + numPrimitivo);
+
+        Integer numWrapper = numPrimitivo;
+        System.out.println("valor primitivo convertido em Integer: " + numWrapper);
+        
+    }
+}
