a9254ef08d5c42ae8fd99cba277b7426ed44ccbb
Pedro Camargo <081camargopedro@gmail.com>
2023-08-31 16:30:37 -0300

    Adicionado tarefa_coleçoes

--- Tarefa_Colecoes/src/App2.java
+++ Tarefa_Colecoes/src/App2.java
@@ -1,36 +1,30 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class App2 {
 
-    public static void main(String Args[]){
+    public static void main(String Args[]) {
 
 
         Scanner entrada = new Scanner(System.in);
 
         System.out.println("Digite nomes e sexos (-m ou -f) separados por vírgula: ");
-        String frase = entrada.nextLine();
-        List lista = new ArrayList<String>(List.of(frase.split(", ")));
+        String todosOsNomes = entrada.nextLine();
+        System.out.println("String: " + todosOsNomes);
 
-        System.out.println("Nomes digitados: "+ lista);
-        Collections.sort(lista);
-        System.out.println("Nomes digitados em ordem alfabética: " +lista);
+        String[] ArraytodosOsNomes = todosOsNomes.split(",");
+        System.out.println("Array com todos os nomes:");
+        System.out.println(Arrays.toString(ArraytodosOsNomes));
 
-        /**
-         * não consegui pensar em nenhuma solução para esse exercício, travei.
-         * agora precisaria dividir a arrayList em 2 por (-m) e (-f) usando o split?
-         * não sei fazer
-         *
-         */
-
-
-
-
-
+        for (String s : ArraytodosOsNomes) {
+            String nome = s.trim(); // Remove espaços em branco no início e no fim
+            if (nome.endsWith("-m")) {
+                System.out.println("Nome: " + nome.substring(0, nome.length() - 2) + ", Sexo: Masculino");
+            } else if (nome.endsWith("-f")) {
+                System.out.println("Nome: " + nome.substring(0, nome.length() - 2) + ", Sexo: Feminino");
+            } else {
+                System.out.println("Nome: " + nome + ", Sexo: Indefinido");
+            }
+        }
     }
-
 }
-
-
