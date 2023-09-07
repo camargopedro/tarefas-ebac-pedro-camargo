95f56f1a49f9c47df058bb0f2a370241da60d4b0
Pedro Camargo <081camargopedro@gmail.com>
2023-09-07 18:15:02 -0300

    Adicionando Projeto1_CRUD

---
+++ Projeto1_CRUD/src/br/com/camargo/dao/IClienteDAO.java
@@ -0,0 +1,20 @@
+package br.com.camargo.dao;
+
+import br.com.camargo.domain.Cliente;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public interface IClienteDAO {
+
+    public Boolean cadastrar(Cliente cliente);
+
+    public void excluir(Long cpf);
+
+    public void alterar(Cliente cliente);
+
+    public Cliente consultar(Long cpf);
+
+    public Collection<Cliente> buscarTodos();
+
+}
